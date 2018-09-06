791a7f1aafab6f0b15916da413603c324ac02405
Omer Doron <omerdoron@Omer-Do-MBP.local>
2018-09-06 23:19:43 +0300

    serial can find the device

--- serial.js
+++ serial.js
@@ -1,25 +1,52 @@
+const { exec } = require('child_process');
 var SerialPort = require('serialport');
-var serialPort = new SerialPort('/dev/cu.usbmodem14321', {
-  baudRate: 9600
-});
 
-let msg = '';
+const piArduinoDevicePrefix = 'ttyACM';
+// usbmodem is for mac
+exec('ls /dev/ | grep -E "(ttyACM|cu.usbmodem).*"', (err, stdout, stderr) => {
+  let devices = stdout.split('\n');
 
-// Switches the port into "flowing mode"
-serialPort.on('data', function(data) {
-  msg = msg + data.toString('utf8');
-  if (data.includes('\n')) {
-    let data = JSON.parse(msg.slice(0, -2));
-
-    if (data.channel) {
-      currentChannel = parseInt(data.channel);
+  console.log(devices);
+  devices.forEach(device => {
+    try {
+      serialListen(device);
+    } catch (err) {
+      console.log('error...');
+      // can't connect to device
     }
-  }
+  });
 });
 
-// Read data that is available but keep the stream from entering //"flowing mode"
-// serialPort.on('readable', function () {
-//   const data = port.read();
-//   console.log(data.toString('utf8'))
-//
-// });
+const serialListen = device => {
+  console.log('device name', `/dev/${device}`);
+  var serialPort = new SerialPort(`/dev/${device}`, {
+    baudRate: 9600
+  });
+
+  let msg = '';
+
+  // Switches the port into "flowing mode"
+  serialPort.on('data', function(data) {
+    try {
+      msg = msg + data.toString('utf8');
+      if (data.includes('\n')) {
+        let data = JSON.parse(msg.slice(0, -2));
+        msg = '';
+
+        if (data.channel) {
+          currentChannel = parseInt(data.channel);
+          console.log('current channel', currentChannel);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // Read data that is available but keep the stream from entering //"flowing mode"
+  // serialPort.on('readable', function () {
+  //   const data = port.read();
+  //   console.log(data.toString('utf8'))
+  //
+  // });
+};
