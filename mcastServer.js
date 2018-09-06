b04b3df3533c9ee80656ff161ebdef9dcb10df94
Omer Doron <omerdoron@Omer-Do-MBP.local>
2018-09-06 23:29:59 +0300

    add callback function to serial

--- mcastServer.js
+++ mcastServer.js
@@ -1,12 +1,21 @@
 const { asyncSleep } = require('./utils');
 const { socket, MULTICAST_ADDR } = require('./mcast');
+const { initSerialListener } = require('./serial');
 
 socket.bind(socket.port);
+
+let currentMode = 1;
+initSerialListener(data => {
+  console.log(data);
+  if (data.channel) {
+    currentMode = parseInt(data.channel);
+  }
+});
 
 const sendMessage = async () => {
   console.log('send message');
   // const message = Buffer.from(`Message from process ${process.pid}`);
-  const msg = JSON.stringify({ mode: '1' });
+  const msg = JSON.stringify({ mode: currentMode });
   for (let i = 0; i < 5; i++) {
     await asyncSleep(2);
     socket.send(msg, 0, msg.length, socket.port, MULTICAST_ADDR, function() {
--- serial.js
+++ serial.js
@@ -1,52 +1,59 @@
 const { exec } = require('child_process');
 var SerialPort = require('serialport');
 
-const piArduinoDevicePrefix = 'ttyACM';
-// usbmodem is for mac
-exec('ls /dev/ | grep -E "(ttyACM|cu.usbmodem).*"', (err, stdout, stderr) => {
-  let devices = stdout.split('\n');
+const initSerialListener = callback => {
+  const piArduinoDevicePrefix = 'ttyACM';
+  // usbmodem is for mac
+  exec('ls /dev/ | grep -E "(ttyACM|cu.usbmodem).*"', (err, stdout, stderr) => {
+    let devices = stdout.split('\n');
 
-  console.log(devices);
-  devices.forEach(device => {
-    try {
-      serialListen(device);
-    } catch (err) {
-      console.log('error...');
-      // can't connect to device
-    }
-  });
-});
-
-const serialListen = device => {
-  console.log('device name', `/dev/${device}`);
-  var serialPort = new SerialPort(`/dev/${device}`, {
-    baudRate: 9600
-  });
-
-  let msg = '';
-
-  // Switches the port into "flowing mode"
-  serialPort.on('data', function(data) {
-    try {
-      msg = msg + data.toString('utf8');
-      if (data.includes('\n')) {
-        let data = JSON.parse(msg.slice(0, -2));
-        msg = '';
-
-        if (data.channel) {
-          currentChannel = parseInt(data.channel);
-          console.log('current channel', currentChannel);
-        }
+    console.log(devices);
+    devices.forEach(device => {
+      try {
+        serialListen(device);
+      } catch (err) {
+        console.log('error...');
+        // can't connect to device
       }
-    } catch (err) {
-      console.log(err);
-    }
+    });
   });
 
-  // Read data that is available but keep the stream from entering //"flowing mode"
-  // serialPort.on('readable', function () {
-  //   const data = port.read();
-  //   console.log(data.toString('utf8'))
-  //
-  // });
+  const serialListen = device => {
+    console.log('device name', `/dev/${device}`);
+    var serialPort = new SerialPort(`/dev/${device}`, {
+      baudRate: 9600
+    });
+
+    let msg = '';
+
+    // Switches the port into "flowing mode"
+    serialPort.on('data', function(data) {
+      try {
+        msg = msg + data.toString('utf8');
+        if (data.includes('\n')) {
+          let data = JSON.parse(msg.slice(0, -2));
+          msg = '';
+
+          if (data.channel) {
+            callback(data);
+            currentChannel = parseInt(data.channel);
+            console.log('current channel', currentChannel);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // Read data that is available but keep the stream from entering //"flowing mode"
+    // serialPort.on('readable', function () {
+    //   const data = port.read();
+    //   console.log(data.toString('utf8'))
+    //
+    // });
+  };
+};
+
+module.exports = {
+  initSerialListener
 };
