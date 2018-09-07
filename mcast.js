f3d1b03b2a69d0074ec4ffce38bd0658c93ca698
Omer Doron <omerdoron@Omer-Do-MBP.local>
2018-09-07 15:21:51 +0300

    sync s3 files into local modes dir

--- mcast.js
+++ mcast.js
@@ -2,6 +2,9 @@ const ip = require('ip');
 const SERVER_IP = ip.address();
 console.log('Device ip address: ', SERVER_IP);
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
 const PORT = 20000;
 const MULTICAST_ADDR = '233.255.255.255';
 
@@ -20,6 +23,21 @@ socket.on('listening', function() {
 socket.port = PORT;
 
 // socket.bind(PORT);
+const syncS3 = async () => {
+  try {
+    let output = await exec(`mkdir modes`);
+  } catch (err) {
+    // folder already exist
+  }
+  try {
+    let output = await exec(`cd modes && aws s3 sync s3://monday-rvr ./ --delete`);
+    console.log(output);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+syncS3();
 
 module.exports = {
   socket,
