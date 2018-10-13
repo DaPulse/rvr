bba60eb60667468a147da56708b8d2a3ee1a249f
Daniel Mittelman <mittelman@campus.technion.ac.il>
2018-10-13 16:02:32 +0300

    Changed VLC behavior to exit completely on playback end

--- mcastClient.js
+++ mcastClient.js
@@ -88,7 +88,7 @@ const playVideoFile = file => {
     vlcProcess = null;
   }
 
-  vlcProcess = spawn('/usr/bin/vlc', ['--fullscreen' ,'--video-on-top', '--no-video-title-show', '--qt-continue=0', '-L', '--play-and-pause', file]);
+  vlcProcess = spawn('/usr/bin/vlc', ['--fullscreen' ,'--video-on-top', '--no-video-title-show', '--qt-continue=0', '--play-and-exit', file]);
 };
 
 socket.bind(socket.port);
